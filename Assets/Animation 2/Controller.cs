99c62b50b910f4ae5225a9639eeee2b4f5c369e9
Blake <telefragent@gmail.com>
2020-01-28 16:06:10 -0500

    Extra scene

---
+++ Assets/Animation 2/Controller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+	public Animator Star;
+
+	public void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.A))
+		{
+			Star.SetFloat("Direction", -1f);
+		}
+		if (Input.GetKeyDown(KeyCode.D))
+		{
+			Star.SetFloat("Direction", 1f);
+		}
+	}
+}

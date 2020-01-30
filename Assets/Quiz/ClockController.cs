be2371037bc721b189197f228fac18f091e443b5
Blake <telefragent@gmail.com>
2020-01-30 12:58:58 -0500

    Prepared For Quiz

---
+++ Assets/Quiz/ClockController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockController : MonoBehaviour
+{
+	public Animator ClockAnimator;
+
+	private void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			
+		}
+	}
+}
